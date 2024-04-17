554a1e1bd4fd84fe1b3cc2872dc06ad2e6f23e89
Ricky Tala <Ricky.Tala@sita.co.za>
2024-04-17 11:37:04 +0200

    First commit, Shopping app

---
+++ Repositories/HomeRepository.cs
@@ -0,0 +1,44 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionEcommerceMVC.Repositories
+{
+    public class HomeRepository : IHomeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public HomeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Category>> Categories()
+        {
+            return await _db.Categories.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts(string sTerm = "", int categoryId = 0)
+        {
+            sTerm = sTerm.ToLower();
+            IEnumerable<Product> products = await (from product in _db.Products
+                                                   join category in _db.Categories
+                                                   on product.CategoryId equals category.Id
+                                                   where string.IsNullOrWhiteSpace(sTerm) || (product != null && product.ProductName.ToLower().StartsWith(sTerm))
+                                                   select new Product
+                                                   {
+                                                       Id = product.Id,
+                                                       Image = product.Image,
+                                                       Brand = product.Brand,
+                                                       ProductName = product.ProductName,
+                                                       CategoryId = product.CategoryId,
+                                                       Price = product.Price,
+                                                       CategoryName = category.CategoryName
+                                                   }
+                         ).ToListAsync();
+            if (categoryId > 0)
+            {
+                products = products.Where(a => a.CategoryId == categoryId).ToList();
+            }
+            return products;
+        }
+    }
+}
